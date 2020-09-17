aa645518bf54601e18ecca87c740c31d9a6acd07
zhuoyangjiang <70708249+zhuoyangjiang@users.noreply.github.com>
2020-09-17 21:49:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-number = input("Eenter temperature in celsius: ")
+number = input("Enter temperature in celsius: ")
 print(f'{number}° in Celsius is equivalent to { float(number)*1.8+32}°Fahrenheit.')
